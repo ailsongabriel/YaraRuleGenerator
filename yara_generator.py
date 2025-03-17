0da6e637e8204de24749d50dda1f3f6aec216f8f
Noslia <57625594+ailsongabriel@users.noreply.github.com>
2025-03-17 14:55:22 -0300

    Add Hex String Search

--- yara_generator.py
+++ yara_generator.py
@@ -1,13 +1,20 @@
 import argparse
 
-def format_string(s, flag, index):
+def format_string(s, flag, index, hex_enabled):
+  hex_string = ' '.join(f'{ord(c):02X}' for c in s) if hex_enabled else None
+  
   if s.startswith('{') and s.endswith('}'):
     return f'$string{index+1} = {s}'
   else:
-    return f'$string{index+1} = "{s}" {flag}'
+    formatted_string = f'$string{index+1} = "{s}" {flag}'
+    if hex_enabled:
+      formatted_string += f'\n      $hex_string{index+1} = {{ {hex_string} }}'
+    return formatted_string
 
-def yaraGen(strings, condition, output_file):
-  defined_strings = "\n      ".join([format_string(s[0], s[1], i) for i, s in enumerate(strings)])
+def yaraGen(strings, condition, output_file, hex_indexes):
+  defined_strings = "\n      ".join([
+      format_string(s[0], s[1], i, str(i) in hex_indexes) for i, s in enumerate(strings)
+  ])
 
   yara_rule = f"""rule {output_file} 
   {{
@@ -27,11 +34,11 @@ def yaraGen(strings, condition, output_file):
 
 def main():
   parser = argparse.ArgumentParser(description="YARA rule generator")
-  parser.add_argument("-p", "--path", required=True, help="Path of the directory to scan")
   parser.add_argument("-o", "--output", default="StringSearch.yar", help="Output file name")
   parser.add_argument("-c", "--condition", choices=["any", "all"], required=True, help="Rule condition (any or all)")
   parser.add_argument("-s", "--strings", nargs='+', required=True, help="List of strings to search for")
   parser.add_argument("-b", "--base64", nargs='*', default=[], help="List of string indexes to be searched in base64 (0-based index)")
+  parser.add_argument("-x", "--hex", nargs='*', default=[], help="List of string indexes to be searched in hexadecimal (0-based index)")
   
   args = parser.parse_args()
   
@@ -39,11 +46,10 @@ def main():
   
   strings = [(s, "base64" if str(i) in args.base64 else "nocase") for i, s in enumerate(args.strings)]
   
-  yaraGen(strings, condition, args.output)
+  yaraGen(strings, condition, args.output, args.hex)
   
-  print("To execute scan with your yara rule, run the following command:")
-  print()
-  print(f'yara {args.output} -r -s {args.path}')
+  print("YARA rule generated successfully and saved in", args.output)
 
 if __name__ == "__main__":
   main()
+
